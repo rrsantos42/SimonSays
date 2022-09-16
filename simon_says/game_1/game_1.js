3f5515c0aa0ede661e3977344cda22d695fa6e37
Ricardo Santos <anaraqsantos@gmail.com>
2022-09-16 16:15:18 +0100

    16/09

--- simon_says/game_1/game_1.js
+++ simon_says/game_1/game_1.js
@@ -9,11 +9,13 @@ SIMON_SAYS.config = [
 	{ css: "sixsix", name: "6x6", length: 36},
 	{ css: "sevenseven", name: "7x7", length:49}
 ];
-
+//global var
 SIMON_SAYS.round_nbr = 0;
 SIMON_SAYS.level_nbr = 0;
+SIMON_SAYS.lifes_counter = 2;
 SIMON_SAYS.cmp_order = [];
 SIMON_SAYS.user_choice = [];
+SIMON_SAYS.keys = [ "twotwo", "threethree", "fourfour", "fivefive", "sixsix", "sevenseven"];
 SIMON_SAYS.selectedGame = undefined;
 SIMON_SAYS.container = undefined;
 SIMON_SAYS.win = document.getElementById("win");
@@ -22,11 +24,21 @@ SIMON_SAYS.lost = document.getElementById("lost");
 SIMON_SAYS.level = document.getElementById("level");
 SIMON_SAYS.round = document.getElementById("round");
 SIMON_SAYS.lifes = document.getElementById("lifes");
+SIMON_SAYS.wait  = document.getElementById("wait");
+SIMON_SAYS.turn  = document.getElementById("turn");
+SIMON_SAYS.start = document.getElementById("start");
+SIMON_SAYS.checkbtn = document.getElementById("next_lvl");
+SIMON_SAYS.nxt_lvl = document.getElementById("nxt_lvl");
 SIMON_SAYS.win.style.display = 'none';
 SIMON_SAYS.lose.style.display = 'none';
 SIMON_SAYS.lost.style.display = 'none';
-SIMON_SAYS.lifes_counter = 2;
+SIMON_SAYS.turn.style.display = 'none';
+SIMON_SAYS.wait.style.display = 'none';
+SIMON_SAYS.checkbtn.style.display = 'none';
+SIMON_SAYS.nxt_lvl.style.display = 'none';
 
+
+// func return the grid 
 SIMON_SAYS.make_grid = function(key) {
 	SIMON_SAYS.container = document.querySelector(".button-container");
 	SIMON_SAYS.selectedGame = SIMON_SAYS.config.find((i)=> { return i.css === key });
@@ -48,39 +60,17 @@ SIMON_SAYS.make_grid = function(key) {
 	SIMON_SAYS.win.style.display = 'none';
 	SIMON_SAYS.lose.style.display = 'none';
 }
-
+// func cleans the container 
 SIMON_SAYS.clear = function() {
 	SIMON_SAYS.container.innerHTML = "";
 	SIMON_SAYS.container.classList.remove(SIMON_SAYS.selectedGame.css);
 }
+
 SIMON_SAYS.sleep = function(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-SIMON_SAYS.clicked_btn = async function(){
-	SIMON_SAYS.wait.style.display = 'block'
-	var x = '0';
-	var tmp = '';
-	var size = SIMON_SAYS.cmp_order.length;
-	while(x < size) 
-	{
-		tmp = SIMON_SAYS.cmp_order[x];
-		document.getElementById(tmp).click();
-		await SIMON_SAYS.sleep(1000);
-		x++;
-	}
-	SIMON_SAYS.user_choice = [];
-	SIMON_SAYS.wait.style.display = 'none';
-	SIMON_SAYS.turn.style.display = 'block';
-}
-
-SIMON_SAYS.round_nbr = 0;
-SIMON_SAYS.user_choice = [];
-
-SIMON_SAYS.round_nbr_add = function(){
-	SIMON_SAYS.round_nbr += 1;
-}
-
+// func that makes a random order so the btn can be clicked 
 SIMON_SAYS.make_order = function(lvl, round_nbr){
 	SIMON_SAYS.cmp_order = [];
 	var i = 0;
@@ -116,8 +106,26 @@ SIMON_SAYS.make_order = function(lvl, round_nbr){
 	}
 	return(SIMON_SAYS.cmp_order);
 }
-SIMON_SAYS.showcontainer = document.getElementById("container");
 
+// func that auto clicks the btn in the cmp_order
+SIMON_SAYS.clicked_btn = async function(){
+	SIMON_SAYS.wait.style.display = 'block'
+	var x = '0';
+	var tmp = '';
+	var size = SIMON_SAYS.cmp_order.length;
+	while(x < size) 
+	{
+		tmp = SIMON_SAYS.cmp_order[x];
+		document.getElementById(tmp).click();
+		await SIMON_SAYS.sleep(1000);
+		x++;
+	}
+	SIMON_SAYS.user_choice = [];
+	SIMON_SAYS.wait.style.display = 'none';
+	SIMON_SAYS.turn.style.display = 'block';
+}
+
+// func that checks if the cmp_order is equal to the user_choice and shows a message acordingly 
 SIMON_SAYS.check = async function(){
 	var size = SIMON_SAYS.cmp_order.length;
 
@@ -125,7 +133,14 @@ SIMON_SAYS.check = async function(){
 	while((SIMON_SAYS.cmp_order[x] == SIMON_SAYS.user_choice[x]) && x < size){
 		x++;
 	}
-	if(x == size){
+	if(x == size && SIMON_SAYS.round_nbr == 2){
+			SIMON_SAYS.nxt_lvl.style.display = 'block';
+			SIMON_SAYS.turn.style.display = 'none';
+			await SIMON_SAYS.sleep(1500);
+			SIMON_SAYS.nxt_lvl.style.display = 'none';
+			SIMON_SAYS.round_nbr += 1;
+		}
+	else if(x == size){
 		console.log("NEXT ROUND");
 		SIMON_SAYS.win.style.display = 'block';
 		SIMON_SAYS.turn.style.display = 'none';
@@ -153,37 +168,32 @@ SIMON_SAYS.check = async function(){
 	SIMON_SAYS.turn.style.display = 'none';
 	SIMON_SAYS.main();
 }
-
-SIMON_SAYS.wait  = document.getElementById("wait");
-SIMON_SAYS.turn  = document.getElementById("turn");
-SIMON_SAYS.turn.style.display = 'none';
-SIMON_SAYS.wait.style.display = 'none';
-SIMON_SAYS.start = document.getElementById("btn");
-SIMON_SAYS.checkbtn = document.getElementById("next_lvl")
-SIMON_SAYS.grids = [ "twotwo", "threethree", "fourfour", "fivefive", "sixsix", "sevenseven"];
-
+// this func sees if the player is on round three and if he is it changes the level to te next one
 SIMON_SAYS.check_rounds = function(){
 
 	if(SIMON_SAYS.round_nbr === 3){
 		SIMON_SAYS.round_nbr = 0;
 		SIMON_SAYS.level_nbr ++;
-		SIMON_SAYS.lifes_counter = 3;
-		SIMON_SAYS.grid_value = SIMON_SAYS.grids[SIMON_SAYS.level_nbr];
+		SIMON_SAYS.lifes_counter = 2;
+		SIMON_SAYS.grid_key = SIMON_SAYS.keys[SIMON_SAYS.level_nbr];
 		
 	}
 	else{
-		SIMON_SAYS.grid_value = SIMON_SAYS.grids[SIMON_SAYS.level_nbr];
+		SIMON_SAYS.grid_key = SIMON_SAYS.keys[SIMON_SAYS.level_nbr];
 	}
 }
+// This is the main func it's executed when the player presses start
 SIMON_SAYS.main = async function(){
 	if(SIMON_SAYS.lifes_counter > 0){SIMON_SAYS.start.style.display = "none";}
-	SIMON_SAYS.level.innerHTML = "Level " + SIMON_SAYS.level_nbr;
-	SIMON_SAYS.round.innerHTML = "Round " + SIMON_SAYS.round_nbr;
-	SIMON_SAYS.lifes.innerHTML = "Lifes " + (SIMON_SAYS.lifes_counter + 1);
-
 	SIMON_SAYS.check_rounds();
-	SIMON_SAYS.make_grid(SIMON_SAYS.grid_value);
+	SIMON_SAYS.make_grid(SIMON_SAYS.grid_key);
 	SIMON_SAYS.make_order(SIMON_SAYS.level_nbr, SIMON_SAYS.round_nbr);
 	await SIMON_SAYS.sleep(1000);
 	SIMON_SAYS.clicked_btn();
+
+	SIMON_SAYS.checkbtn.style.display = 'block'; 
+	SIMON_SAYS.level.innerHTML = "Level " + (SIMON_SAYS.level_nbr + 1);
+	SIMON_SAYS.round.innerHTML = "Round " + (SIMON_SAYS.round_nbr + 1);
+	SIMON_SAYS.lifes.innerHTML = "Lifes " + (SIMON_SAYS.lifes_counter + 1);
+
 }
