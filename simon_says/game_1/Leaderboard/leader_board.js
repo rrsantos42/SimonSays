e72f309d271c1aa478b5d8062dd25133c3cca524
Ricardo Santos <anaraqsantos@gmail.com>
2022-09-28 02:43:25 +0100

    28/09/22

---
+++ simon_says/game_1/Leaderboard/leader_board.js
@@ -0,0 +1,51 @@
+const colors = {
+	mercedes: '#00D2BE',
+	ferrari: '#DC0000',
+
+  }
+
+// target the table element in which to add one div for each driver
+const main = d3
+  .select('table');
+
+// for each driver add one table row
+// ! add a class to the row to differentiate the rows from the existing one
+// otherwise the select method would target the existing one and include one row less than the required amount
+const drivers = main
+  .selectAll('tr.driver')
+  .data(leaderboard)
+  .enter()
+  .append('tr')
+  .attr('class', 'driver');
+
+// in each row add the information specified by the dataset in td elements
+// specify a class to style the elements differently with CSS
+
+// position using the index of the data points
+drivers
+  .append('td')
+  .text((d, i) => i + 1)
+  .attr('class', 'position');
+
+
+// name followed by the team
+drivers
+  .append('td')
+  // include the last name in a separate element to style it differently
+  // include the team also in another element for the same reason
+  .html (({name, team}) => `${name.split(' ').map((part, index) => index > 0 ? `<strong>${part}</strong>` : `${part}`).join(' ')} <span>${team}</span>`)
+  // include a border with the color matching the team
+  .style('border-left', ({team}) => {
+    // find the color using the string value found in d.team
+    // ! if the string value has a space, camelCase the value
+    const color = team.split(' ').map((word, index) => index > 0 ? `${word[0].toUpperCase()}${word.slice(1)}` : `${word}` ).join('');
+    return `4px solid ${colors[color]}`;
+  })
+  .attr('class', 'driver');
+
+// gap from the first driver
+drivers
+  .append('td')
+  .attr('class', 'gap')
+  .append('span')
+  .text(({gap}) => gap);q
