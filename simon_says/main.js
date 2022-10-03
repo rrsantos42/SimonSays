0d91548132a53384f2143e3d0722f12054d30125
Ricardo Santos <anaraqsantos@gmail.com>
2022-10-03 23:42:23 +0100

    commit para amanha

--- simon_says/main.js
+++
@@ -1,3 +0,0 @@
-function ft_login(){
-	window.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=3b7fb6225c6c6e9d1c699f8f3d7758e7651ad63a9299aa25aad6d5b7f1dc5814&redirect_uri=https%3A%2F%2Frrsantos42.github.io%2Fsimon_says%2Fgame_1%2Fgame_1.html&response_type=code";
-}
